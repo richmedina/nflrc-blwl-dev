ddc5eb2c08eb144ff43ec15781b232ae498016cb
Richard Medina <rmedina@Ruby.local>
2014-12-24 10:00:16 -1000

    template html safe

--- lessons/migrations/0007_merge.py
+++ lessons/migrations/0007_merge.py
@@ -8,7 +8,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('lessons', '0006_auto_20141223_1258'),
-        ('lessons', '0006_auto_20141222_1341'),
     ]
 
     operations = [
