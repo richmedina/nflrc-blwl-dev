b2a1b77436ca7861415517437a035695ad738110
nflrc system admin <nflrcsa@nflrc-server-x.mcl>
2014-12-24 09:44:49 -1000

    syncing

---
+++ lessons/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lessons', '0006_auto_20141223_1258'),
+        ('lessons', '0006_auto_20141222_1341'),
+    ]
+
+    operations = [
+    ]
