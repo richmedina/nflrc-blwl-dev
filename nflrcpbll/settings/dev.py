092ea87b75eb0a93aee5827cf99896362a5899ba
r@m <richmedina@gmail.com>
2015-11-08 10:52:38 -0700

    rebooting setup

--- nflrcpbll/settings/dev.py
+++ nflrcpbll/settings/dev.py
@@ -12,7 +12,7 @@ TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS += (
-	'debug_toolbar',
+    'debug_toolbar',
 )
 
 DATABASES = {
@@ -23,14 +23,14 @@ DATABASES = {
 }
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ['SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET']
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ['SOCIAL_AUTH_GOOGLE_OAUTH2_KEY'] # This is the Client ID (not a key)
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ['SOCIAL_AUTH_GOOGLE_OAUTH2_KEY']  # This is the Client ID (not a key)
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_WHITELISTED_EMAILS = []
 SOCIAL_AUTH_USERNAME_IS_FULL_EMAIL = True
 
 SOCIAL_AUTH_LOGIN_ERROR_URL = '/login-forbidden/'
 
-LOGIN_URL ='/login/google-oauth2'
+LOGIN_URL = '/login/google-oauth2'
 LOGIN_REDIRECT_URL = '/'
 
 MEDIA_ROOT = '/Library/WebServer/Documents/media'
